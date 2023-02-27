add490f6db9c8c041e1a41cf5e0357344cf8f7a8
abrilgrassi8 <abrilmorenni@hotmail.com>
2023-02-27 12:45:47 -0300

    se modificó navbar completo

--- src/Components/CartWidget/CartWidget.jsx
+++ src/Components/CartWidget/CartWidget.jsx
@@ -4,7 +4,7 @@ import { BsFillCartCheckFill } from "react-icons/bs"
 const CartWidget = () => {
   return (
     <div>
-        <BsFillCartCheckFill size={30} marginRight={10}/>
+        <BsFillCartCheckFill size={30} marginRight={10} onClick/>
     </div>
   );
 };
